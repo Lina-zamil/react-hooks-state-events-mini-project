772f9f899283ddf4586feee2824b942c3db0d85a
Lina-zamil <lina.abumahfood@gmail.com>
2024-01-18 17:00:39 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+
 
 function App() {
+  const [categories, setCategories] = useState(CATEGORIES);
+  const [tasks, setTasks] = useState(TASKS);
+  const [categoryBtn, setCategoryBtn] = useState("All");
+
+  function itemDelete(itemToDelete) {
+    setTasks(tasks.filter((item) => item.text !== itemToDelete));
+  }
+
+  function addNewItem(newListItem) {
+    setTasks([...tasks, newListItem]);
+  }
+
+
+
+  const displayedItems = tasks.filter((item) => {
+    if (categoryBtn === "All") {
+      return true;
+    } else {
+      return categoryBtn === item.category;
+    }
+  });
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        selectBtn={setCategoryBtn}
+        onBtn={categoryBtn}
+        categories={categories}
+      />
+      <NewTaskForm onTaskFormSubmit={addNewItem} categories={categories} />
+      <TaskList tasks={displayedItems} itemDelete={itemDelete} />
     </div>
   );
 }
